efaeb0927a7939e568818398268b0d51c9d37d41
kologermit <kologermit@gmail.com>
2024-02-26 20:24:52 +0400

    Vector: push_back

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <iostream>
+#include <exception>
 // T имеет:
 // 1) Стандартный конструктор
 // 2) Конструктор копирования
@@ -7,26 +8,26 @@
 template<typename T>
 class Vector {
 private:
-    T* begin_ptr;
+    T* _begin_ptr;
     size_t _size;
-    size_t _cap;
-    size_t get_capacity_from_size(const size_t& size) const;
-    void init(const size_t& size);
+    size_t _capacity;
+    static size_t get_capacity_from_size(const size_t& size);
+    void init(const size_t& size, const size_t& cap = -1);
 public:
-    class OutOfRange {
+    class OutOfRangeException: public std::exception {
     public:
-        const char* what() const {
+        const char* what() const noexcept override {
             return "Error: Index out of range";
         }
     };
-    const T& operator[](const int& index);
+    const T& operator[](const size_t& index) const;
+    T& operator[](const size_t& index);
     ~Vector();
-    Vector();
-    Vector(const size_t size);
-    Vector(const size_t size, const T& val);
+    Vector(const size_t size = 0, const T& val = T());
     void push_back(const T& val);
     void pop_back();
     size_t size() const;
+    size_t capacity() const;
 
 public:
     // std::random_access_iterator_tag
@@ -38,20 +39,20 @@ public:
 };
 
 template<typename T>
-size_t Vector<T>::get_capacity_from_size(const size_t& size) const {
-    size_t cap = 1;
-    while (cap / 2 < size) {
-        cap *= 2;
+size_t Vector<T>::get_capacity_from_size(const size_t& size) {
+    size_t capacity = 1;
+    while (capacity < size) {
+        capacity *= 2;
     }
-    return cap;
+    return capacity;
 }
 
 template<typename T>
-void Vector<T>::init(const size_t& size) {
-    size_t cap = this->get_capacity_from_size(size);
-    this->begin_ptr = new T[cap];
+void Vector<T>::init(const size_t& size, const size_t& cap) {
+    size_t capacity = cap == -1 ? this->get_capacity_from_size(size) : cap;
+    this->_begin_ptr = new T[capacity];
     this->_size = size;
-    this->_cap = cap;
+    this->_capacity = capacity;
 }
 
 template<typename T>
@@ -60,38 +61,51 @@ size_t Vector<T>::size() const {
 }
 
 template<typename T>
-const T& Vector<T>::operator[](const int& index) {
+size_t Vector<T>::capacity() const {
+    return this->_capacity;
+}
+
+template<typename T>
+const T& Vector<T>::operator[](const size_t& index) const {
+    if (index >= this->_size || index < 0) {
+        throw OutOfRangeException();
+    }
+    return this->_begin_ptr[index];
+}
+
+template<typename T>
+T& Vector<T>::operator[](const size_t& index) {
     if (index >= this->_size) {
-        throw OutOfRange();
+        throw OutOfRangeException();
     }
-    return *(this->begin_ptr + index);
-}
-
-template<typename T>
-Vector<T>::Vector() {
-    this->init(0);
-    for (int i = 0; i < size; ++i) {
-        this->begin_ptr[i] = T();
-    }
-}
-
-template<typename T>
-Vector<T>::Vector(const size_t size) {
-    this->init(size);
-    for (int i = 0; i < size; ++i) {
-        this->begin_ptr[i] = T();
-    }
+    return this->_begin_ptr[index];
 }
 
 template<typename T>
 Vector<T>::Vector(const size_t size, const T& val) {
     this->init(size);
-    for (int i = 0; i < size; ++i) {
-        this->begin_ptr[i] = val;
+    for (size_t i = 0; i < size; ++i) {
+        this->_begin_ptr[i] = val;
     }
 }
 
 template<typename T>
 Vector<T>::~Vector() {
-    delete this->begin_ptr;    
+    delete[] this->_begin_ptr;    
+}
+
+template<typename T>
+void Vector<T>::push_back(const T& val) {
+    if (this->_size + 1 == this->_capacity) {
+        T* tmp_ptr = this->_begin_ptr;
+        this->init(this->_size + 1, this->_capacity * 2);
+        for (size_t i = 0; i < this->_size - 1; ++i) {
+            this->_begin_ptr[i] = tmp_ptr[i];
+        }
+        this->_begin_ptr[this->_size - 1] = val;
+        delete[] tmp_ptr;
+        return;
+    }
+    this->_begin_ptr[this->_size] = val;
+    this->_size++;
 }
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -2,6 +2,15 @@
 #include "Vector.h"
 using namespace std;
 int main() {
-    Vector<int> v(10, 2);
-    cout << v[2] << endl;
+    Vector<int> v(0);
+    cout << "v.size: " << v.size() << endl;
+    cout << "v.capacity: " << v.capacity() << endl;
+    cout << "v: "; for (int i = 0; i < v.size(); i++) cout << v[i] << " "; cout << endl;
+    for (int i = 0; i < 10; i++) {
+        v.push_back(i);
+    }
+    cout << "Push...\n";
+    cout << "v.size: " << v.size() << endl;
+    cout << "v.capacity: " << v.capacity() << endl;
+    cout << "v: "; for (int i = 0; i < v.size(); i++) cout << v[i] << " "; cout << endl;
 }
