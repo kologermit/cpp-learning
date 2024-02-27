dd7fd2016911b4e1a4e17624da1be5fb8a0aca52
kologermit <kologermit@gmail.com>
2024-02-27 22:22:42 +0400

    Vector: main.cpp

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include "Vector.h"
-// #include <vector>
 using namespace std;
+struct A {
+    int a, b, c;
+    A(int a = 0, int b = 0, int c = 0): a(a), b(b), c(c) {}
+    friend std::ostream& operator<<(std::ostream& out, const A& a) {
+        return out << "(" << a.a << " " << a.b << " " << a.c << ")";
+    }
+    ~A() {
+        cout << "destr" << endl;
+    }
+};
 int main() {
-    Vector<int> v(0);
+    Vector<A> v(0);
     cout << "v.size: " << v.size() << endl;
     cout << "v.capacity: " << v.capacity() << endl;
     cout << "v: "; for (int i = 0; i < v.size(); i++) cout << v[i] << " "; cout << endl;
-    v.pop_back();
     for (int i = 0; i < 10; i++) {
-        v.push_back(i);
+        v.emplace_back(1, 2, 3);
     }
     cout << "Push...\n";
     cout << "v.size: " << v.size() << endl;
