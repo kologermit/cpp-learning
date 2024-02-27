ac5286724aceda5e41706dac8387203b0046a8b3
kologermit <kologermit@gmail.com>
2024-02-27 22:12:32 +0400

    Vector: emplace_back

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -13,6 +13,7 @@ private:
     size_t _capacity;
     static size_t get_capacity_from_size(const size_t& size);
     void init(const size_t& size, const size_t& cap = -1);
+    void double_memory();
 public:
     class OutOfRangeException: public std::exception {
     public:
@@ -25,6 +26,8 @@ public:
     ~Vector();
     Vector(const size_t size = 0, const T& val = T());
     void push_back(const T& val);
+    template<typename... Args>
+    void emplace_back(Args&&... args);
     void pop_back();
     size_t size() const;
     size_t capacity() const;
@@ -53,6 +56,16 @@ void Vector<T>::init(const size_t& size, const size_t& cap) {
     this->_begin_ptr = new T[capacity];
     this->_size = size;
     this->_capacity = capacity;
+}
+
+template<typename T>
+void Vector<T>::double_memory() {
+    T* tmp_ptr = this->_begin_ptr;
+    this->init(this->_size + 1, this->_capacity * 2);
+    for (size_t i = 0; i < this->_size - 1; ++i) {
+        this->_begin_ptr[i] = tmp_ptr[i];
+    }
+    delete[] tmp_ptr;
 }
 
 template<typename T>
@@ -97,16 +110,23 @@ Vector<T>::~Vector() {
 template<typename T>
 void Vector<T>::push_back(const T& val) {
     if (this->_size + 1 == this->_capacity) {
-        T* tmp_ptr = this->_begin_ptr;
-        this->init(this->_size + 1, this->_capacity * 2);
-        for (size_t i = 0; i < this->_size - 1; ++i) {
-            this->_begin_ptr[i] = tmp_ptr[i];
-        }
+        this->double_memory();
         this->_begin_ptr[this->_size - 1] = val;
-        delete[] tmp_ptr;
         return;
     }
     this->_begin_ptr[this->_size] = val;
+    ++this->_size;
+}
+
+template<typename T>
+template<typename... Args>
+void Vector<T>::emplace_back(Args&&... args) {
+    if (this->_size + 1 == this->_capacity) {
+        this->double_memory();
+        this->_begin_ptr[this->_size - 1] = T(args...);
+        return;
+    }
+    this->_begin_ptr[this->_size] = T(args...);
     ++this->_size;
 }
 
