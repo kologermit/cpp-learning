1b1d75695ba911771fef7419b1e937da07b7c75f
Feodor <kologermit@gmail.com>
2024-03-11 19:18:54 +0400

    AVL: print

--- AVL/AVL.h
+++ AVL/AVL.h
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <functional>
 
+int main();
+
 template<typename KeyType, typename CompareType = std::less<KeyType>>
 class AVLTree {
 private:
@@ -18,12 +20,15 @@ private:
             static bool find(const Node* node, const KeyType& key);
             static size_t size(const Node* node);
             static size_t height(const Node* node);
+            static size_t update_height(Node* node);
             void insert(const KeyType& key);
+            static void print(const Node* node);
 
         friend class AVLTree;
     };
     Node* _root;
     friend class Node;
+    friend int main();
 public:
 
     AVLTree(): _root(nullptr) {};
@@ -32,6 +37,7 @@ public:
     void erase(const KeyType& value);
     bool find(const KeyType& value) const;
     size_t size() const;
+    size_t height() const;
 };
 
 template<typename KeyType, typename CompareType>
@@ -83,18 +89,37 @@ size_t AVLTree<KeyType, CompareType>::Node::size(const Node* node) {
 
 template<typename KeyType, typename CompareType>
 size_t AVLTree<KeyType, CompareType>::Node::height(const Node* node) {
+    if (node) {
+        return node->_height;
+    }
+    return 0;
+}
+
+template<typename KeyType, typename CompareType>
+size_t AVLTree<KeyType, CompareType>::Node::update_height(Node* node) {
     if (!node) {
         return 0;
     }
-    return 1 + node->_height + 
-        std::max(
-            node->height(node->_left),
-            node->height(node->_right)
-        );
+    return (node->_height = std::max(update_height(node->_left), update_height(node->_right)) + 1);
+}
+
+template<typename KeyType, typename CompareType>
+void AVLTree<KeyType, CompareType>::Node::print(const Node* node) {
+    if (!node) {
+        return;
+    }
+    std::cout 
+        << "key: " << node->_key << "; left: " 
+        << (node->_left ? node->_left->_key : 0) << "; right: " 
+        << (node->_right ? node->_right->_key : -0) << "; height: "
+        << node->_height << std::endl;
+    print(node->_left);
+    print(node->_right);
 }
 
 template<typename KeyType, typename CompareType>
 void AVLTree<KeyType, CompareType>::Node::insert(const KeyType& key) {
+    Node* new_node = nullptr;
     if (less(key, this->_key)) {
         if (!this->_left) {
             this->_left = new Node(key);
@@ -118,6 +143,11 @@ bool AVLTree<KeyType, CompareType>::find(const KeyType& key) const {
 template<typename KeyType, typename CompareType>
 size_t AVLTree<KeyType, CompareType>::size() const {
     return Node::size(this->_root);
+}
+
+template<typename KeyType, typename CompareType>
+size_t AVLTree<KeyType, CompareType>::height() const {
+    return Node::height(this->_root);
 }
 
 template<typename KeyType, typename CompareType>
--- AVL/main.cpp
+++ AVL/main.cpp
@@ -4,11 +4,10 @@ using namespace std;
 int main() {
     AVLTree<int> tree;
     for (int i = 0; i < 10; i++) {
-        tree.insert(i);
+        tree.insert((i % 2 ? -i : i));
     }
     cout << "size: " << tree.size() << endl;
-    // cout << "height: " << tree.height() << endl;
-    for (int i = 0; i < 10; i++) {
-        cout << i << " "<< tree.find(i) << endl;
-    }
+    cout << "height: " << tree.height() << endl;
+    AVLTree<int>::Node::update_height(tree._root);
+    AVLTree<int>::Node::print(tree._root);
 }
